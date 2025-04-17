211b1c126e41302aa4773216e9c3b69ea40309ab
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2025-04-17 11:52:44 +0530

    Put Method

--- backend/src/main/java/com/example/backend/Controllers/BlogController.java
+++ backend/src/main/java/com/example/backend/Controllers/BlogController.java
@@ -3,14 +3,17 @@ package com.example.backend.Controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.backend.Models.PostModel;
 import com.example.backend.Repository.PostRepository;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -27,6 +30,11 @@ public class BlogController {
     public List<PostModel> getBlogs() {
         return postRepo.findAll();
     }
+   
+    @GetMapping("/api/Posts/{id}")
+    public Optional<PostModel> getBlogById(@PathVariable Long id) {
+        return postRepo.findById(id);
+    }
 
     @PostMapping("/api/Posts")
     public String postBlog(@RequestBody PostModel newPost) {
@@ -34,6 +42,23 @@ public class BlogController {
         return "Post Added Successfully";
     }
     
+    @PutMapping("/api/Posts/{id}")
+    public String putBlog(@PathVariable Long id, @RequestBody PostModel newPost) {
+    PostModel existingPost = postRepo.findById(id).orElse(null);
+
+    if (existingPost == null) {
+        return "Post Not Found with ID: " + id;
+    }
+
+    existingPost.setBlogTitle(newPost.getBlogTitle());
+    existingPost.setBlogDesc(newPost.getBlogDesc());
+    existingPost.setBlogAuthor(newPost.getBlogAuthor());
+    
+
+    postRepo.save(existingPost);
+    return "Post Updated Successfully";
+}
+
     
     
 }
